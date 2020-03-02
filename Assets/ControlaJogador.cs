7cbade1024386518a5d515026cb2e8764ac7773f
ribeirogab <gblosr@gmail.com>
2020-03-02 00:30:53 -0300

    player movimentation

--- Assets/ControlaJogador.cs
+++ Assets/ControlaJogador.cs
@@ -14,5 +14,14 @@ public class ControlaJogador : MonoBehaviour
         Vector3 direction = new Vector3(eixoX, 0, eixoZ);
 
         transform.Translate(direction * Speed * Time.deltaTime);
+
+        if (direction != Vector3.zero)
+        {
+            GetComponent<Animator>().SetBool("Movendo", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Movendo", false);
+        }
     }
 }
